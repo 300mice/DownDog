93d966e46377c2a3aaee03acccd1e3c66bc71dcf
BenAnderson <31300554+300mice@users.noreply.github.com>
2026-05-31 13:28:40 +1000

    Item Class
    Grab only costs stamina if grabbing an item atm (no attachment yet)

---
+++ Source/DownDog/Item/Item.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item.h"
+
+#include "AbilitySystemComponent.h"
+
+// Sets default values
+AItem::AItem()
+{
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Item Mesh"));
+	SetRootComponent(ItemMesh);
+	
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	ASC = CreateDefaultSubobject<UAbilitySystemComponent>("ASC");
+	ASC->SetIsReplicated(true);
+	ASC->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
+}
+
+// Called when the game starts or when spawned
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	ASC->InitAbilityActorInfo(this, this);
+}
+
---
+++ Source/DownDog/Item/Item.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+class UAbilitySystemComponent;
+
+UCLASS()
+class DOWNDOG_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	//Components
+protected:
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category="Components")
+	UStaticMeshComponent* ItemMesh;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category="Components")
+	UAbilitySystemComponent* ASC;
+
+};

0b28dd4043805b95ef5a82725f72513fbfaeb5dc
BenAnderson <31300554+300mice@users.noreply.github.com>
2026-06-18 18:16:12 +1000

    Mover speed attribute

---
+++ Source/DownDog/Mover/DownDogWalkingMode.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mover/DownDogWalkingMode.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "DownDogChaosMoverComponent.h"
+#include "AbilitySystem/Attributes/DownDogPlayerSet.h"
+
+class UDownDogPlayerSet;
+class UAbilitySystemComponent;
+
+float UDownDogWalkingMode::GetMaxSpeed() const
+{
+	UAbilitySystemComponent* ASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetMoverComponent()->GetOwner());
+	if (!ASC) return Super::GetMaxSpeed();
+	const UDownDogPlayerSet* PlayerAttributeSet = Cast<UDownDogPlayerSet>(ASC->GetAttributeSet(UDownDogPlayerSet::StaticClass()));
+	if (!PlayerAttributeSet) return Super::GetMaxSpeed();
+
+	return PlayerAttributeSet->GetMovementSpeed();
+}
